e96f7d80fa4a01cda3c268a9696d63bad7e21e2d
Steve Elliott <steve@tegud.net>
2016-08-20 13:17:35 +0100

    Fancy Slack message

--- send-to-slack/index.js
+++ send-to-slack/index.js
@@ -4,6 +4,14 @@ const AWS = require("aws-sdk");
 const https = require("https");
 const slackWebhookPath = "%SLACK_WEBHOOK_PATH%";
 
+const propertyFieldTitles = {
+    url: 'URL',
+    statusCode: 'Status Code',
+    errorMessage: 'Error',
+    timeToFirstByte: 'Time to First Byte',
+    timeout: 'Timeout'
+};
+
 exports.handler = function (event, context, callback) {
     const result = JSON.parse(event.Records[0].Sns.Message);
 
@@ -16,7 +24,26 @@ exports.handler = function (event, context, callback) {
     });
 
     req.write(JSON.stringify({
-        "text": `Site check result: ${result.url} ${result.success ? 'was successful' : 'failed'}.`
+        attachments: [
+            {
+            	"fallback": "Site check result: ${result.url} ${result.success ? 'was successful' : 'failed'}. ${result.errorMessage}",
+                "text": `Site check result: ${result.url} ${result.success ? 'was successful' : 'failed'}.`,
+            	"color": result.success ? "good" : "danger",
+            	"fields": Object.keys(propertyFieldTitles).reduce((fields, currentProperty) => {
+                    if(!result[currentProperty]) {
+                        return fields;
+                    }
+
+                    fields.push({
+            			"title": propertyFieldTitles[currentProperty],
+            			"value": result[currentProperty],
+            			"short": true
+            		});
+
+                    return fields;
+                }, [])
+            }
+        ]
     }))
     req.end();
 };
