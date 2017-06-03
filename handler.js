eded845af85770d8578222f754bed7c0b68816d0
Steve Elliott <steve@tegud.net>
2017-06-03 11:54:52 +0100

    start of serverless

---
+++ handler.js
@@ -0,0 +1,11 @@
+module.exports.makeRequest = (event, context, callback) => {
+    callback(null, response);
+};
+
+module.exports.handleRequest = (event, context, callback) => {
+    callback(null, response);
+};
+
+module.exports.sendToSlack = (event, context, callback) => {
+    callback(null, response);
+};
